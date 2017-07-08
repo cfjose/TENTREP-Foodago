e97b8661f4ef592831002eec2457e1a76e0b0660
Danya Balcena <dpbalcena@gmail.com>
2017-07-08 13:28:29 +0800

    Added design for frontend homepage

--- Application/foodago/frontend/assets/AppAsset.php
+++ Application/foodago/frontend/assets/AppAsset.php
@@ -13,8 +13,22 @@ class AppAsset extends AssetBundle
     public $baseUrl = '@web';
     public $css = [
         'css/site.css',
+		'css/animate.css',
+		'css/bootstrap.css',
+		'css/style.css',
     ];
     public $js = [
+		'js/classie.js',
+		'js/easing.js',
+		'js/jquery.carouFredSel-6.1.0-packed.js',
+		'js/jquery.easydropdown.js',
+		'js/jquery.flexisel.js',
+		'js/jquery.min.js',
+		'js/move-top.js',
+		'js/simpleCart.min.js',
+		'js/tms-0.4.1.js',
+		'js/uisearch.js',
+		'js/wow.min.js',
     ];
     public $depends = [
         'yii\web\YiiAsset',

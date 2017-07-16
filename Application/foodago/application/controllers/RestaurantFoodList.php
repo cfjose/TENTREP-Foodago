45a9edcafd492486926115600527671db700b2db
Chamber Jose <chamberj18@gmail.com>
2017-07-16 22:11:58 +0800

    Minor Bug Fixes

--- Application/foodago/application/controllers/RestaurantFoodList.php
+++ Application/foodago/application/controllers/RestaurantFoodList.php
@@ -1,7 +1,7 @@
 <?php
 	defined('BASEPATH') OR exit('No direct script access allowed');
 
-	class TrackOrder extends CI_Controller{
+	class RestaurantFoodList extends CI_Controller{
 		public function index(){
 			$this->load->view('restaurantfoodlist');
 		}

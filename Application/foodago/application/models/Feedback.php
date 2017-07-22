8b9addc8292b4522bc959d72a8ceea9d810de24e
Jose Lorenzo Tadeo <renzotadeo26@gmail.com>
2017-07-22 23:07:41 +0800

    added logic in review

--- Application/foodago/application/models/Feedback.php
+++ Application/foodago/application/models/Feedback.php
@@ -2,6 +2,13 @@
 	defined('BASEPATH') OR exit('No direct script access allowed');
 
 	class Feedback extends CI_Model{
-		
+		public function getFoodItemFeedbackCount($data){
+            $this->db->select('feedback_id');
+            $this->db->from('food_items_has_feedback');
+            $this->db->where("food_items_id ='" . $data . "'");
+
+            $query = $this->db->get();
+            return $query;
+        }
 	}
 ?>

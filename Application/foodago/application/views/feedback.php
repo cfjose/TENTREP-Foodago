1a2d6b00221e60a436de23740818d41a59dad123
jcheramia7 <johannaheramia@yahoo.com>
2017-07-24 20:11:12 -0700

    updated feedback page

--- Application/foodago/application/views/feedback.php
+++ Application/foodago/application/views/feedback.php
@@ -1,8 +1,234 @@
+
+<!DOCTYPE html>
 <html>
 <head>
-	<title>Review</title>
+	<<link rel="icon" href="<?php echo base_url(); ?>assets/images/global/favicon.ico">
+	<title>Foodago</title>
+
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="<?php echo base_url(); ?>css/bootstrap.min.css">
+	<link rel="stylesheet" href="<?php echo base_url(); ?>css/animate.css">
+	<link rel="stylesheet" href="<?php echo base_url(); ?>css/boostrap.css">
+	<link rel="stylesheet" href="<?php echo base_url(); ?>css/style.css">
+	<link rel="stylesheet" href="<?php echo base_url(); ?>css/site.css">
+
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+    <link rel="stylesheet" href="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css">
+    <script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
+    <script src="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
+
+	<!-- jQuery library -->
+	<script src="<?php echo base_url(); ?>/js/jquery.min.js"></script>
+
+	<!-- Latest compiled JavaScript -->
+	<script src="<?php echo base_url(); ?>/js/bootstrap.min.js"></script>
+
+	<style>
+		.carousel-inner{
+			height: 600px;
+		}
+
+		.header{ 
+			max-height: 600px; 
+			width: 100%;
+			filter: brightness(40%);
+		}
+
+		.search{
+			top: 25%;
+			left: 10%;
+			width: 75%;
+			position: absolute;
+		}
+
+		h1, h2{
+			color: #DEB675;
+			font-family: "Arial";
+		}
+
+		h5{
+			color: black;
+		}
+
+		#q{
+			height: 50px;
+			width: 75%;
+			padding: 10px;
+			border-radius: 5px;
+			font-size: 15px;
+		}
+
+		.sidebar{
+			width: 100%;
+		}
+
+		.category-nav{
+			width: 30%;
+			margin:10px;
+		}
+
+		.food-item-list{
+			width: 100%;
+			left: 25%;
+			top: 35%;
+			min-height:100px;
+			margin-left:-380px;
+		}
+
+		.subcategory-pane{
+			height: 300px;
+		}
+
+		.sub-category-names{
+			text-indent: 5%;
+			line-height: 30px;
+		}
+
+		.no-avail{
+			text-align: center;
+			vertical-align: middle;
+			margin-top: 10%;
+		}
+
+		.caret{
+			color: orange;
+		}
+
+		.dropdown{
+			margin: 3px;
+			right: 25%;
+		}
+		.hr0{
+			margin: 5px;
+			color: black;
+		}
+
+		.panel-default{
+			border-color: black;
+			color: black;
+		}
+		
+		a{
+			text-decoration: none;
+		}
+		.count-input {
+		  position: relative;
+		  width: 70%;
+		  max-width: 165px;
+		  margin: 10px 0;
+		}
+		.count-input input {
+		  width: 70%;
+		  height: 10px;
+		  border-radius: 2px;
+		  background: none;
+		  text-align: center;
+		}
+		.count-input input:focus {
+		  outline: none;
+
+		}
+		.count-input .incr-btn {
+		  display: block;
+		  position: absolute;
+		  width: 30px;
+		  height: 10px;
+		  font-size: 16px;
+		  font-weight: 300;
+		  text-align: center;
+		  line-height: 30px;
+		  top: 50%;
+		  right: 0;
+		  margin-top: -15px;
+		  text-decoration:none;
+		  border-radius: 15px;
+		}
+		.count-input .incr-btn:first-child {
+		  right: auto;
+		  left: 0;
+		  top: 46%;
+		}
+		.count-input.count-input-sm {
+		  max-width: 125px;
+		}
+		.count-input.count-input-sm input {
+		  height: 36px;
+		}
+		.count-input.count-input-lg {
+		  max-width: 200px;
+		}
+		.count-input.count-input-lg input {
+		  height: 70px;
+		  border-radius: 3px;
+		}
+		.btn-white{
+			background-color: transparent;
+			padding: 0px;
+			margin-top: 20px;
+		}
+
+	</style>
 </head>
 
-<body></body>
+<body>
+	<nav class="navbar-inverse navbar-fixed-top">
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<a class="navbar-brand" href="#"><img src="<?php echo base_url(); ?>/assets/images/global/logos/logoName.png" alt=""></a>
+	    	</div>
+	    	<ul class="nav navbar-nav navbar-right">
+		    	<div class="dropdown">
+		    		<!--<label class="username"><?php echo $this->session->userdata('username'); ?></label>-->
+		    		<img src="<?php echo base_url('assets/images/main/icons/user.png'); ?>" height="45" width="45" data-toggle="dropdown"/>
+					<span class="caret"></span>
+					<ul class="dropdown-menu">
+						<p align="center">You are currently logged in as <b><?php echo $this->session->userdata('first_name') . " " . $this->session->userdata('last_name'); ?></b></p>
+						<li class="divider"></li>
+						<li><a href="<?php echo base_url(); ?>index.php/profile">My Profile</a></li>
+						<li><a href="<?php echo base_url(); ?>index.php/profile/accountSettings">Account Settings</a></li>
+						<li><a href="<?php echo base_url(); ?>index.php/login/logout">Logout</a></li>
+					</ul>
+	  			</div>
+	    	</ul>
+	  	</div>
+	</nav>
+	<div class="carousel-inner">
+		<div class="item active">
+			<img src="<?php echo base_url(); ?>assets/images/main/main-bg.jpg" alt="" class="header">
+			<div class="search">
+				<form method="get" action="<?php base_url(); ?>index.php/search">
+					<h1 id="greetings"></h1>
+					<script type="text/javascript">
+						window.onload = function(){
+							var currDate = new Date();
+							var currTime = currDate.getHours();
+
+							var greet;
+
+							if(currTime < 12){
+								greet = 'Good Morning';
+							}else if(currTime >= 12 && currTime <= 17){
+								greet = 'Good Afternoon';
+							}else if(currTime >=17 && currTime <= 24){
+								greet = 'Good Evening';
+							}
+
+							document.getElementById("greetings").innerHTML = greet + ", " + <?php echo json_encode($this->session->userdata('first_name')) ?>;
+						}
+					</script>
+					<h2>Are you Hungry?</h2>
+					<h2>Search for your favorite restaurants / fast food chains online</h2><br/>
+					<input type="text" id="q" placeholder="Search for Restaurants and Fast food Chains"/>
+				</form>
+			</div>
+	  	</div>
+	</div>
+</body>
 
 </html>

c571402f8763d26f1b861d0b896b8e145d8721ba
jcheramia7 <johannaheramia@yahoo.com>
2017-09-04 16:38:29 -0700

    updated modal for order transcript

--- Application/foodago/application/views/order_transcript.php
+++ Application/foodago/application/views/order_transcript.php
@@ -264,15 +264,14 @@
 							<div class="modal-content">
 								<div class="modal-header">
 									<button type="button" class="close" data-dismiss="modal">&times;</button>
-									<h4 class="modal-title">WARNING! </h4>
-									<label class="switch">
-										Are you sure you want to cancel your order?
-									</label>
-								</div>
-								<div class="modal-body">
+									<h3 class="modal-title"><b>WARNING! </b></h3>
 
 								</div>
+								<div class="modal-body">
+										<h4>Are you sure you want to cancel your order?</h4>
+								</div>
 								<div class="modal-footer">
+									<button type="button" class="btn btn-danger" data-dismiss="modal">YES</button>
 									<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
 								</div>
 							</div>
